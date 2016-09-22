29da88dd2d4223cb2ef88c5d4c9ae7d0465b3a4c
Marica Antonacci <marica.antonacci@gmail.com>
2016-09-22 09:59:47 +0200

    Fixing neutron options in horizon

--- manifests/horizon.pp
+++ manifests/horizon.pp
@@ -9,6 +9,16 @@ class openstack_integration::horizon {
     # need to disable offline compression due to
     # https://bugs.launchpad.net/ubuntu/+source/horizon/+bug/1424042
     compress_offline   => false,
+
+
+    neutron_options => {
+#      'enable_lb' => true,
+      'enable_firewall' => true,
+#      'enable_vpn' => true,
+      'enable_router' => false,
+      'enable_quotas' => true,
+      'enable_security_group' => true,
+    },
   }
 
 }
